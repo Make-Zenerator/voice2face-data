bd1905ed3d3db778aab9f86a2a9bd67e23eaac92
dlatjgus0612 <dlatjgus0612@gmail.com>
2024-03-16 22:34:18 +0900

    Feat : Add code for extracting objects from an img
    - Using the zero-shot CLIPSEG2 API from Hugging Face to perform inference locally.
    - #11

--- code/image_removebg.py
+++
@@ -1,6 +0,0 @@
-from rembg import remove
-from PIL import Image
-
-input = Image.open('/root/project/voice2face-data/file/face_detected_256x256.png') # load image
-output = remove(input) # remove background
-output.save('/root/project/voice2face-data/file/rembg_256x256.png') # save image
