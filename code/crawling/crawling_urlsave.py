ad30a723656896e9365a80d0176305ecd596afb8
Im._.SH <85490226+dlatjgus0612@users.noreply.github.com>
2024-03-30 12:15:26 +0900

    Feat/images (#18)
    * Feat : Add age, gender model
    
    -
    - #5
    
    * Fix : Modify model path
    
    -
    - #5
    
    * Feat : add Captioning for labeling
    
    - Pick a good frame from face recognition with opencv
    - #7
    
    * Fix : Modify file name (age index)
    - #5
    
    * Fix : centerinfo normalization
    
    -
    -
    
    * Feat : Add relabeling csv
    
    - #5
    -
    
    * Fix : Modify class name
    
    - #5
    -
    
    * Fix : modify valiable name
    
    - #5
    -
    
    * Feat : lLink main function
    
    - #5
    -
    
    * Remove : duplicated file
    
    -
    -
    
    * Style : remmove duplicated variable
    
    -
    - #5
    
    * Fix : file name format
    
    - #5
    
    * Feat : Age classification for VoxCeleb
    
    - #5
    
    * [FEAT] airflow (#9)
    
    * Feat : Add age, gender model
    
    -
    - #5
    
    * Fix : Modify model path
    
    -
    - #5
    
    * Fix : Modify file name (age index)
    - #5
    
    * Fix : centerinfo normalization
    
    -
    -
    
    * Feat : Add relabeling csv
    
    - #5
    -
    
    * Fix : Modify class name
    
    - #5
    -
    
    * Fix : modify valiable name
    
    - #5
    -
    
    * Feat : lLink main function
    
    - #5
    -
    
    * Remove : duplicated file
    
    -
    -
    
    * Style : remmove duplicated variable
    
    -
    - #5
    
    * Feat : Add Dockerfile & modify folders
    
    - Add Dockerfile & add Airflow files
    - Modify folders architecture simply
    - #8
    
    ---------
    
    Co-authored-by: carboxaminoo <carboxamino@daum.net>
    
    * Feat : Trimming user Audio files
    
    - Refining the user's voice
    - #10
    
    * Feat : Add Trimming users Audio files
    
    - Refining user audio received front
    - #10
    
    * Rename : Add Comments & Renaming
    
    - Rebuilding cods
    -
    
    * Feat : Add files
    
    - add codes about image, video ...
    - #11
    
    * Feat : Add code for extracting objects from an img
    
    - Using the zero-shot CLIPSEG2 API from Hugging Face to perform inference locally.
    - #11
    
    * Feat : Add tar unzip and wav_croping
    
    - #14
    
    * Feat : Voxceleb dataset relabeling
    
    - #5
    
    * Feat : tar file unzip and remove
    
    - #14
    
    * Feat: AVspeech dataset relabeling
    
    - #5
    
    * Rename : move file labeling to OLKAVS
    
    - #14
    
    * Rename : change file name spelling error
    
    - #14
    
    * Comment : add docstring
    
    - #14
    
    * Docs : modified gitignore
    
    -
    -
    
    * Feat : Extract image from video
    
    - #16
    
    * Fix : Change from one to all mp4 in the folder
    
    - #17
    
    * Comment : Add docsting
    
    * Docs : fix commit message
    
    -
    -
    
    * Docs : modify docs
    
    - modify docs
    -
    
    * Rename : Organize folder locations
    
    * Rename : Modify file name
    
    * Docs : V.1 README
    
    - Add README
    -
    
    ---------
    
    Co-authored-by: carboxaminoo <carboxamino@daum.net>
    Co-authored-by: carboxaminoo <carboxaminoo@daum.net>

--- code/crawling/crawling_urlsave.py
+++ code/crawling/crawling_urlsave.py
@@ -8,17 +8,23 @@ import time
 import pandas as pd
 import os
 
-# WebDriver 초기화 (PATH에 추가했기 때문에 executable_path를 사용하지 않음)
+'''
+YouTube crawling using Selenium
+
+Saves information of approximately 162 videos including name, title, and URL to Youtube_search_df.csv.
+'''
+
+# Initialize WebDriver (executable_path not required as it's added to PATH)
 browser = webdriver.Chrome()
 
-# 접속 url
+# URL to access
 url = "https://youtube.com/"
 
-# 검색 키워드
-keyword = "나혼자 여행"
+# Search keyword
+keyword = "Solo Travel"
 
-# 스크롤을 어디까지 내리는지 기준 
-# finish_line = 40000 기준: 162 개
+# Scroll until the specified line
+# finish_line = 40000 (about 162 videos)
 finish_line = 10000
 
 browser.maximize_window()
@@ -29,65 +35,63 @@ time.sleep(2)
 search.send_keys(keyword)
 search.send_keys(Keys.ENTER)
 
-# 검색 후 url 작업창 변경 (파싱)
+# Switch to search result page for parsing
 present_url = browser.current_url
 browser.get(present_url)
 last_page_height = browser.execute_script("return document.documentElement.scrollHeight")
 
-# 스크롤 100번 수행
+# Scroll 100 times
 scroll_count = 0
 while scroll_count < 100:
-    # 우선 스크롤 내리기
+    # Scroll down
     browser.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
-    time.sleep(2.0)       # 작업 중간에 1이상으로 간격을 줘야 데이터 취득가능(스크롤을 내릴 때의 데이터 로딩 시간 때문)
-    # 현재 위치 담기
+    time.sleep(2.0)       # Waiting for data to load when scrolling down
     new_page_height = browser.execute_script("return document.documentElement.scrollHeight")
     
-    # 스크롤 횟수 증가
+    # Increase scroll count
     scroll_count += 1
 
 html_source = browser.page_source
 soup = BeautifulSoup(html_source, 'html.parser')
 
-# finish line까지 모든 검색 결과 정보 가져오기
-# 모든 컨텐츠 관련 부분을 떼어내기
-# find_all: 해당 정보의 모든 부분 가져오기
+# Retrieve all search results up to the finish line
+# Extract all content-related sections
 elem = soup.find_all("ytd-video-renderer", class_="style-scope ytd-item-section-renderer")
 
-# 필요한 정보 가져오기
+# Retrieve necessary information
 df = []
-for t in elem[:100]:  # 처음 100개의 동영상 정보만 가져오도록 수정
+for t in elem[:100]:  # Retrieve only the first 100 video information
     title = t.find("yt-formatted-string", class_="style-scope ytd-video-renderer").get_text()
     name = t.find("a", class_="yt-simple-endpoint style-scope yt-formatted-string").get_text()
     content_url = t.find("a", class_="yt-simple-endpoint style-scope ytd-video-renderer")["href"]
     df.append([name, title , 'https://www.youtube.com/'+content_url])
 
-## 자료 저장
-# 데이터 프레임 만들기
+## Save data
+# Create DataFrame
 new = pd.DataFrame(columns=['name', 'title' , 'url_link'])
 
-# 자료 집어넣기
+# Insert data
 for i in range(len(df)):
     new.loc[i] = df[i]
 
-# 데이터를 저장할 디렉토리 생성
+# Create directory to save data
 df_dir = "./data/"
 if not os.path.exists(df_dir):
     os.makedirs(df_dir)
 
-# 저장하기
-new.to_csv(os.path.join(df_dir, "Youtube_search_df.csv"), index=True, encoding='utf8')  # 인덱스 포함하여 저장
+# Save data
+new.to_csv(os.path.join(df_dir, "Youtube_search_df.csv"), index=True, encoding='utf8')  # Save with index
 
-## 컬럼 정보 저장
-# 컬럼 설명 테이블
+## Save column information
+# Column description table
 col_names = ['name', 'title' ,'url_link']
-col_exp = ['컨텐츠 올린 채널명', '컨텐츠 제목', '연결 링크']
+col_exp = ['Channel name', 'Video title', 'URL link']
 
 new_exp = pd.DataFrame({'col_names':col_names,
                         'col_explanation':col_exp})
 
-# 저장하기
+# Save
 new_exp.to_csv(os.path.join(df_dir, "Youtube_col_exp.csv"), index=False, encoding='utf8')
 
-# 브라우저 닫기
+# Close the browser
 browser.close()
